f80f2293a582494f8986930da4297337b9df5312
katieden <39695640+katieden@users.noreply.github.com>
2021-04-25 23:01:24 +0300

    app and Student

---
+++ app.py
@@ -0,0 +1,10 @@
+from Student import Student
+
+student1 = Student('Peter', 'Biology', 5, True)
+student2 = Student('Jim', 'History', 2, False)
+
+
+print(student1.name)
+print(student2.gpa)
+
+print(student1.on_honor_roll())
