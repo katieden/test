2efa51c6df5c54d6cce408fc7dbf877512297667
katieden <39695640+katieden@users.noreply.github.com>
2021-04-25 22:54:24 +0300

    add puz

--- 1.py
+++ 1.py
@@ -1,7 +1,17 @@
-print('main commit1')
-print('main 2 commit')
+from random import randint
 
 
+list1 = []
+length = int(input('Enter number '))
+for i in range(length):
+    list1.append(randint(1, 100))
 
-print ('branch1 commit1')
-print('branch1 2 commit')
+
+for i in range(length):
+    for j in range(i, length):
+        if list1[i] > list1[j]:
+            temp = list1[j]
+            list1[j] = list1[i]
+            list1[i] = temp
+
+print(list1)
